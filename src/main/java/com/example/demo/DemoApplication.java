a543127e8dd9c934cda5ffcf4c5f70cba0dc7cac
Rajeshhatwar <Rajeshhatwar2027@gmail.com>
2024-05-09 21:43:20 +0530

    Resolved the conflict

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // Testing
-// Oka 
+// Okay 
 // Author: Rajesh
+
 
 @SpringBootApplication
 public class DemoApplication {
